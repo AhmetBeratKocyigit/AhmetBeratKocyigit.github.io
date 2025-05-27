3b099952d4fb7f18597391daef8742f111bda1bb
Ahmet Berat Koçyiğit <135528027+AhmetBeratKocyigit@users.noreply.github.com>
2025-05-27 14:17:41 +0300

    Add Yemeksepeti

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function ToMain() {
+    window.location.href = "index.html";
+}
+function ToFood() {
+    window.location.href = "yemekler.html";
+}
+function ToSite() {
+    window.location.href = "https://www.yemeksepeti.com/";
+}
