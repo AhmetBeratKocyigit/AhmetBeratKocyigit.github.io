286a7a9e7039d3517298f9a05a7f341c5a42da2b
Ahmet Berat Koçyiğit <135528027+AhmetBeratKocyigit@users.noreply.github.com>
2025-05-28 21:58:51 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function ToMain() {
-    window.location.href = "index.html";
-}
-function ToFood() {
-    window.location.href = "yemekler.html";
-}
-function ToSite() {
-    window.location.href = "https://www.yemeksepeti.com/";
-}
